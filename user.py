1b60f75fd0759cfc062a706185b0c780c4c7a969
shrek1love <c0lbarator@noreply.codeberg.org>
2023-04-29 17:00:38 +0300

    upload userpy

--- user.py
+++ user.py
@@ -21,31 +21,140 @@ from aiogram_dialog.widgets.kbd import Button, ScrollingGroup
 from appwrite.query import Query
 from operator import itemgetter
 from typing import Any
+import math
+from numpy import deg2rad
+async def compute_Delta(degrees):
+    return math.pi / 180.0 * 6371210 * math.cos(deg2rad(degrees))
+
+
+#https://en.wikipedia.org/wiki/Longitude#Length_of_a_degree_of_longitude
+
+
 API_TOKEN = '1026624360:AAGAI3gKXOhwwC3gEoVdm9tIBFCVRPekJek'
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(storage=MemoryStorage())
-'''dp.include_router(dialog_create)
-dp.include_router(dialog_edit)
-setup_dialogs(dp)'''
-
+dp.include_router(wsetting)
+setup_dialogs(dp)
+databases = Databases(client)
+dbid, ucid = '644c2f1074ca81e7f813', '644cf9f1d76be85bbdd4'
+#Начало создания мероприятия
 @dp.message(Command('start'))
 async def send_welcome(message: types.Message):
     builder = InlineKeyboardBuilder()
     builder.add(types.InlineKeyboardButton(
         text="Настройки профиля",
-        callback_data="create_event", )
+        callback_data="settings", )
     )
     builder.add(types.InlineKeyboardButton(
-        text='Редактировать мероприятие',
-        callback_data="edit_event"
+        text='Список предстоящих мероприятий',
+        callback_data="event_list"
     ))
-    await message.reply("Привет!\nТы открыл официального бота Тула Куда пойти!\nЗдесь ты найдёшь интересные мероприятия города\n", reply_markup=builder.as_markup())
 
-@dp.callback_query(Text("create_event"))
-async def cc(callback: types.CallbackQuery, dialog_manager: DialogManager):
-     await dialog_manager.start(event_creation.ch_event_name, mode=StartMode.RESET_STACK)
-@dp.callback_query(Text("edit_event"))
-async def ec(callback: types.CallbackQuery, dialog_manager: DialogManager):
-     await dialog_manager.start(event_editing.ch_event_choose, mode=StartMode.RESET_STACK)
+    if len(databases.list_documents(dbid, cid, Query.equal('userid', message.from_user.id))) == 0:
+        response = databases.create_document(
+                dbid,
+                ucid,
+                document_id=ID.unique(),
+                data={
+                    'userid':message.from_user.id,
+                }
+        )
+    await message.reply("Привет!\nТы открыл официального бота Тула Куда пойти!\nЗдесь ты найдёшь интересные мероприятия города\n", reply_markup=builder.as_markup())
+class setting(StatesGroup):
+    ch_mpage = State()
+    ch_place = State()
+    ch_radius = State()
+async def chplace_handler(message: Message, message_input: MessageInput,
+                             manager: DialogManager):
+    if manager.is_preview():
+        await manager.next()
+        return
+    #did = manager.dialog_data.get('did', '')
+    #result = databases.update_document(dbid, cid, did, {'datetime':str(datetime.fromisoformat(str(manager.dialog_data.get("newdate", ""))+' '+message.text).isoformat())})
+    userid = manager.event.from_user.id
+    did = databases.list_documents(dbid, ucid, [Query.equal('userid', userid)])['documents']['$id']
+    manager.dialog_data["did"] = did
+    response = databases.update_document(dbid, ucid, did, {'location':[message.location.longitude, message.location.latitude]})
+    await manager.switch_to(event_editing.ch_editing)
+async def chplace_handler(message: Message, message_input: MessageInput,
+                             manager: DialogManager):
+    if manager.is_preview():
+        await manager.next()
+        return
+    #did = manager.dialog_data.get('did', '')
+    #result = databases.update_document(dbid, cid, did, {'datetime':str(datetime.fromisoformat(str(manager.dialog_data.get("newdate", ""))+' '+message.text).isoformat())})
+    DISTANCE = int(message.text)*1000; #Интересующее нас расстояние
+
+    #https://en.wikipedia.org/wiki/Longitude#Length_of_a_degree_of_longitude
+    #did = databases.list_documents(dbid, ucid, [Query.equal('userid', userid)])['documents'][0]['$id']
+    longitude = float(databases.list_documents(dbid, ucid, [Query.equal('userid', userid)])['documents'][0]['location'][0]) #Интересующие нас координаты широты. Потом будут импортироваться из тг.
+    latitude = float(databases.list_documents(dbid, ucid, [Query.equal('userid', userid)])['documents'][0]['location'][1]) #Интересующие нас координаты долготы. Потом будут импортироваться из тг.
+    deltaLon = compute_Delta(longitude); # Дельту по долготе
+    deltaLat = compute_Delta(latitude); #Получаем дельту по широте
+    aroundLat = DISTANCE / deltaLat; # Вычисляем диапазон координат по широте
+    aroundLon = DISTANCE / deltaLon; # Вычисляем диапазон координат по долготе
+    #did = databases.list_documents(dbid, ucid, [Query.equal('userid', userid)])['documents']['$id']
+    #manager.dialog_data["did"] = did
+
+    response = databases.update_document(dbid, ucid, did, {'cordirange':[longitude-aroundLon/2, longitude+aroundLon/2, latitude-aroundLat/2, latitude+aroundLat/2]})
+    await manager.switch_to(event_editing.ch_editing)
+chplace = Window(
+    Const('Пришли местоположение'),
+    MessageInput(chplace_handler)
+)
+chradius = Window(
+    Const('Пришли радиус поиска, например, 5, где 5 - число километров'),
+    MessageInput(chradius_handler)
+)
+mainwindow = Window(
+    Const('Ты зашёл в настройки бота. Здесь ты можешь поменять местоположение и радиус поиска'),
+    Button('Местоположение', on_click=SwitchTo(setting.ch_place)),
+    Button('Радиус поиска', on_click=SwitchTo(setting.ch_radius))
+)
+setting_dialog = Dialog(mainwindow, chplace, chradius)
+async def event_getter(dialog_manager: DialogManager,**_kwargs):
+    userid = dialog_manager.event.from_user.id
+    kritery = databases.list_documents(dbid, ucid, [Query.equal('userid', userid)])['documents']['cordirange']
+    a = databases.list_documents(dbid, ucid, [Query.lessthan()])['documents']
+    names = []
+    ids = []
+    for el in a:
+        tmp = {}
+        tmp['name'] = el['name']
+        tmp['id'] = el['$id']
+        names.append(tmp)
+        #ids.append(el['$id'])
+    #dialog_manager.dialog_data["ids"] = ids
+    return {
+        'products':names,
+    }
+event_choose = Window(
+    Const('Все перемены к лучшему! Выбери необходимое мероприятие из списка'),
+    ScrollingGroup(
+        Select(
+            Format("{item[name]}"),
+            id='s_events',
+            items='products',
+            item_id_getter=lambda x:x['id'],
+            on_click=on_fruit_selected,
+        ),
+        Button(
+            Const('Назад'),
+            on_click=Back(),
+            id='eeback'
+        ),
+        width=1,
+        height=5,
+        id='scroll_with_pager'
+    ),
+    getter=tbc_getter,
+    state=event_editing.ch_event_choose
+)
+@dp.callback_query(Text("settings"))
+async def cc(callback: types.CallbackQuery, manager: DialogManager):
+    await manager.start(setting.ch_place, mode=StartMode.RESET_STACK)
+@dp.callback_query(Text("event_list"))
+async def el(callback: types.CallbackQuery, manager: DialogManager):
+    await manager.start(setting.ch_place, mode=StartMode.RESET_STACK)
 if __name__ == '__main__':
     dp.run_polling(bot, skip_updates=True)
